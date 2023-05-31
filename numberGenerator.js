79605b824e531aae20e832482626375922020e29
Lucas Walter Marquardt <40346279+Erodraeda@users.noreply.github.com>
2023-05-31 20:48:24 -0300

    bublé

---
+++ numberGenerator.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+let numbers = ''
+
+for (let i = 0; i < 131072; i++) {
+    numbers += (`${between(1, 100000)},`);
+}
+
+function between(min, max) {  
+    return Math.floor(
+      Math.random() * (max - min) + min
+    )
+}
+
+fs.writeFileSync('./numb.txt', numbers, err => {
+    if (err) console.error('duh')
+})
